17723be5c5fbfd1a1941b4780a34f5ae1632f626
Eduardo Flores <81997573+edurflores@users.noreply.github.com>
2022-06-11 03:03:01 -0300

    Update claseLista.py

--- Ejercicio6/claseLista.py
+++ Ejercicio6/claseLista.py
@@ -7,7 +7,7 @@ class Lista:
     __indice = 0
     __tope = 0
 
-    __numeronodo = 0 ### Numero que se adjuntara al nodo para indicar su posicion en la lista
+    __numeronodo = 0 ### Numero de nodos de la lista.
 
     def __init__(self):
         self.__comienzo = None
@@ -37,10 +37,45 @@ class Lista:
 
     def insertaAparato(self,unAparato,xpos): ### Inserta un aparato en una posicion valida determinada
         if type(xpos) is int:
-            if xpos < self.__numeronodo:
-                nodo = Nodo(unAparato,xpos)
+            if self.__tope == 0: ### La lista esta vacia. Se lo inserta al principio.
+                print('Lista vacia detectada. Se hara insercion al principio.')
+                nodo = Nodo(unAparato,0)
+                self.__numeronodo += 1
                 nodo.setSiguiente(self.__comienzo)
-                ###Ver lista de programacion procedural
+                self.__comienzo = nodo
+                self.__actual = nodo
+                self.__tope += 1
+                print('Se ha insertado el nodo al principio de la lista. Se ignoro posicion dada por el usuario.')
+            elif xpos < self.__tope: ### La lista no esta vacia. Se hara insercion en la posicion solicitada.
+                    nodo = Nodo(unAparato,xpos)
+                    nodo.setSiguiente(self.__comienzo)
+                    aux = self.__comienzo ### Se resguarda la lista de las modificaciones
+                    anterior = nodo ### Para hacer insercion adentro o al final.
+                    while aux != None and nodo.getPosicion() < xpos: ### Recorrido
+                        anterior = aux
+                        aux = aux.getSiguiente()
+                    anterior.setSiguiente(nodo) ### Se inserta el nuevo nodo en el medio
+                    nodo.setSiguiente(aux)
+                    while aux != None: ### Incrementa en 1 la posicion de los nodos que siguen
+                        aux.incrementaPosicion()
+                        aux = aux.getSiguiente()
+                    self.__tope += 1 ### Al insertarse un nodo mas, se incrementa el total de nodos
+                    print('Nodo insertado.')
+
+    def buscaPosicion(self,xpos): ### Inciso 3, devuelve el dato en la posicion buscada
+        aux = self.__comienzo
+        encontrado = False
+        dato = None
+        if type(xpos) is int:
+            if xpos < self.__tope:
+                while aux != None and encontrado == False:
+                    if aux.getPosicion() == xpos:
+                        dato = aux.getDato()
+                        encontrado = True
+                    else:
+                        aux = aux.getSiguiente()
+        return dato
+
 
     def listarDatosAparatos(self):
         aux = self.__comienzo
